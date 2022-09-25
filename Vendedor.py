39d635e3f2c2eaf4af7bb2270c484a5d9d302f9c
LuckyK <88355964+aL0ver@users.noreply.github.com>
2022-09-25 17:17:24 -0500

    Create Vendedor.py

---
+++ Vendedor.py
@@ -0,0 +1,43 @@
+# Creamos la clase Vendedor
+class Vendedor:
+    # Creamos el constructor
+    def __init__(self, nombre: str, *ventas ) -> None: 
+        self.__nombre =  nombre
+        self.__ventas = ventas
+    # Creamos el metodo getter
+    def getVentasDelUsuario(self) -> None:
+        return self.__ventas
+    def setVentas(self, indice: int, valor: float) -> None:
+        # Nos ubicamos en el indice que nos proporcionaron dentro de la lista y colocamos el valor entregado
+        self.__ventas[indice] = valor
+    # Un setter comun y corriente
+    def setNombre(self, n: str):
+        self.__nombre =  n
+    # Recorremos la lista e imprimos cada valor
+    def imprimeVentasAnuales(self) -> None:
+        for venta in self.__ventas:
+            print(venta)
+    # Recorremos la lista de ventas y vamos sumando cada valor a una variable contadora, dspues se imprime la variable.
+    def totalVentasAnuales(self) -> float: 
+        aux = 0.0
+        # Un parametro *args mete todos los valores en una tupla, si se la pasa una lista, esta va a conservar su propiedad de lista, por lo que si se quiere acceder a ella se debera hacer con un for anidado
+        for venta in self.__ventas:
+            for j in venta:
+                    aux += j
+        return aux
+
+vendedores = [] # Creamos esta lista para guardar los objetos que crearemos a continuacion
+# Utilizamos un bucle while ya que es el usuario el que decide en que momento detener el programa
+while True:
+    nombre = input('Nombre del vendedor: ')
+    # Creamos un generador para facilitar la recoleccion de datos de venta anual
+    ventas = [float(input(f'Valor de venta del mes {i+1}: ')) for i in range(2)] # El numero dentro del range es la cantidad de peticiones, en este caso el numero de meses en un año
+    vendedor = Vendedor(nombre, ventas) # El objeto de clase Vendedor
+    vendedores.append(vendedor)
+    decicion = int(input('Desea continuar el programa? Si(1) No(0)\n> '))
+    if decicion == 0:
+        break
+valor_acumulado = 0.0
+for vendedor in vendedores:
+    valor_acumulado += vendedor.totalVentasAnuales()
+print(f'Ventas totales durante la ejecucion del programa: ${valor_acumulado} ')
